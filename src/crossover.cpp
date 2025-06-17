883323bb4d345789d56bb9d1f611176a89635b11
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-17 17:52:22 +0100

    Updated code

--- src/crossover.cpp
+++ src/crossover.cpp
@@ -1,1 +1,15 @@
+#include "crossover.h"
 
+bool Crossover::eventIndexMatches(uint16_t index) {
+
+  return false;
+}
+
+bool Crossover::eventIndexMatchesCurrentState(uint16_t index) {
+
+  return false;
+}
+
+void Crossover::sendEventsForCurrentState() {
+
+}
--- src/crossover.h
+++ src/crossover.h
@@ -1,1 +1,48 @@
+#ifndef CROSSOVER_H
+#define CROSSOVER_H
 
+#include <Arduino.h>
+#include "LCC_Node_Component_Base.h"
+#include <vector>
+
+class Position_Crossover {
+  public:
+    Position_Crossover(uint8_t positionNumber,
+                      const char *positionDescription,
+                      uint16_t eventMove,
+                      uint16_t eventLeaving,
+                      uint16_t eventReached);
+
+
+
+  private:
+    uint8_t positionNumber;
+    const char *positionDescription;
+    uint16_t eventMove;
+    uint16_t eventLeaving;
+    uint16_t eventReached;
+};
+
+class Crossover : public LCC_Node_Component_Base {
+  public:
+    void addPosition(uint8_t positionNumber,
+                      const char *positionDescription,
+                      uint16_t eventMove,
+                      uint16_t eventLeaving,
+                      uint16_t eventReached);
+
+
+
+    bool eventIndexMatches(uint16_t index) override;
+    bool eventIndexMatchesCurrentState(uint16_t index) override;
+    void sendEventsForCurrentState() override;
+
+
+  private:
+      uint16_t eventToggle;
+
+      std::vector<Position_Crossover> positions;
+
+};
+
+#endif
